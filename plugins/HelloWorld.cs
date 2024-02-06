93558af181294d77289a10ffd328a68454b680e8
Ods Scott <ods@rustwipeday.com>
2024-02-05 20:37:29 -0500

    Initial commit

---
+++ plugins/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿namespace Carbon.Plugins
+{
+    [Info("HelloWorld", "<author>", "1.0.0")]
+    [Description("<optional_description>")]
+    public class HelloWorld : CarbonPlugin
+    {
+        private void OnServerInitialized()
+        {
+            Puts("Hello world!");
+        }
+
+        void OnPlayerConnected(BasePlayer player)
+        {
+            Server.Broadcast($"Hello {player.displayName}");
+        }
+    }
+}
